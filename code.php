2775e5ad93183506f0642e2816b1f523874283eb
gogomalchev <53188133+gogomalchev@users.noreply.github.com>
2021-10-05 15:26:21 +0200

    Update code.php

--- code.php
+++ code.php
@@ -4,18 +4,27 @@
 
 <?php
 if(isset($_POST['mail'])) {
-$email_to      = 'mujemail@gmail.com';
-$sender         = "email2@example.cz";
-$headers        = 'From:' . $sender;
+  $email_to       = 'gogomalchevmc@gmail.com';
+  $sender         = "info@dispdps.cz";
+  $email_to2      = 'kuba1.cienciala@seznam.cz';
+  $sender2        = "info@dispdps.cz";
+  $headers        = 'From:' . $sender;
+  $headers2       = 'From:' . $sender2;
 
 $email_subject = "Test mail";
 $email_body = "Hello! This is a simple email message.";
 
 
 if(mail($email_to, $email_subject, $email_body, $headers)){
-    echo "The email($email_subject) was successfully sent.";
+  echo "The email1($email_subject) was successfully sent.";
 } else {
-    echo "The email($email_subject) was NOT sent.";
+  echo "The email1($email_subject) was NOT sent.";
+}
+
+if(mail($email_to2, $email_subject, $email_body, $headers2)){
+  echo "The email2($email_subject) was successfully sent.";
+} else {
+  echo "The email2($email_subject) was NOT sent.";
 }
 }
           
